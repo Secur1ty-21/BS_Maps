384c63fcd1f9a0b8951e789958444ae7d30b5cd5
Secur1ty-21 <den.bykov.02@mail.ru>
2021-03-29 17:04:35 +0300

    Adding the ability to scale an image with an artifact

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,56 +3,72 @@ package com.example.myapplication
 import android.app.Activity
 import android.graphics.*
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
 import android.widget.ImageView
-import androidx.core.view.drawToBitmap
+
 
 
 class MainActivity : Activity() {
-    private var firstfloor: ImageView? = null
-    private var way:ImageView? = null
-    private var mX : Float = 0.0f
-    private var mY : Float = 0.0f
+    private var scale = 1f
+    //private var touchMatrix: Matrix? = Matrix()
+    private var scaleDetector: ScaleGestureDetector? =  null
     //UI
+    private var firstFloor: ImageView? = null
+    private var way: ImageView? = null
     var myBitmap = Bitmap.createBitmap(600, 800, Bitmap.Config.ARGB_8888);
+    //
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MyLog", "this")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        firstfloor = findViewById(R.id.firstfloor)
-        way = findViewById(R.id.bitmap)
-      
-        //var myBitmap = firstfloor?.drawToBitmap(Bitmap.Config.ARGB_8888)
 
+        firstFloor = findViewById(R.id.firstfloor)
+        way = findViewById(R.id.bitmap)
+        scaleDetector = ScaleGestureDetector(this, ScaleListener())
         var canvas = Canvas(myBitmap)
         var paint = Paint(Paint.ANTI_ALIAS_FLAG)
-        paint.setColor(Color.BLACK)
+        paint.color = Color.BLACK
         canvas.drawBitmap(myBitmap, 0f, 0f, null)
         canvas.drawCircle(100f, 100f, 100f, paint)
-        firstfloor?.draw(canvas)
         way?.setImageBitmap(myBitmap)
-
-
-
-
         way?.scaleX = 3.0f
         way?.scaleY = 3.0f
-        firstfloor?.scaleX = 3.0f
-        firstfloor?.scaleY = 3.0f
-        firstfloor?.setOnTouchListener(PrivateOnTouchListener())
+        firstFloor?.scaleX = 3.0f
+        firstFloor?.scaleY = 3.0f
+        firstFloor?.setOnTouchListener(PrivateOnTouchListener())
     }
-//    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
-//
-//    }
-    private inner class PrivateOnTouchListener : View.OnTouchListener {
+
+    private inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(detector : ScaleGestureDetector) : Boolean{
+            scale *= detector.scaleFactor
+            //Log.d("MyLog", "detector ${detector.scaleFactor}")
+            //Log.d("MyLog", "scale1 $scale")
+            scale = Math.max(1.0f, Math.min(scale, 3f))
+            //Log.d("MyLog", "scale2 $scale")
+            runOnUiThread{
+                firstFloor?.scaleX = scale
+                firstFloor?.scaleY = scale
+                way?.scaleX = scale
+                way?.scaleY = scale
+            }
+            return true
+        }
+    }
+
+     private inner class PrivateOnTouchListener : View.OnTouchListener{
+
         private var getLastX = 0.0f
         private var getLastY = 0.0f
         private var upLastX = 0.0f
         private var upLastY = 0.0f
-        private var zumm = firstfloor?.scaleX!! * 0.5f
+        private var zoom = firstFloor?.scaleX!! * 0.5f
         override fun onTouch(v: View, event: MotionEvent): Boolean {
-                var getX = event.x
-                var getY = event.y
+                scaleDetector?.onTouchEvent(event)
+                val getX = event.x
+                val getY = event.y
                 when(event.action){
                     MotionEvent.ACTION_DOWN -> {
                         getLastX = event.x
@@ -62,32 +78,33 @@ class MainActivity : Activity() {
 //                        }
                     }
                     MotionEvent.ACTION_MOVE -> {
-                        var deltaX = getX - getLastX
-                        var deltaY = getY - getLastY
-                        if (deltaX + upLastX < 300f * zumm && deltaX + upLastX > -300f * zumm && deltaY + upLastY < 200f * zumm && deltaY + upLastY > -200f * zumm) {
+                        val deltaX = getX - getLastX
+                        val deltaY = getY - getLastY
+                        if (deltaX + upLastX < 300f * zoom && deltaX + upLastX > -300f * zoom && deltaY + upLastY < 200f * zoom && deltaY + upLastY > -200f * zoom) {
                             runOnUiThread {
-                                firstfloor?.scrollX = (deltaX.toInt() + upLastX.toInt()) * -1
-                                firstfloor?.scrollY = (deltaY.toInt() + upLastY.toInt()) * -1
+                                firstFloor?.scrollX = (deltaX.toInt() + upLastX.toInt()) * -1
+                                firstFloor?.scrollY = (deltaY.toInt() + upLastY.toInt()) * -1
                                 way?.scrollX = (deltaX.toInt() + upLastX.toInt()) * -1
                                 way?.scrollY = (deltaY.toInt() + upLastY.toInt()) * -1
 
                             }
+
                         }
                     }
                     MotionEvent.ACTION_UP -> {
                         upLastX += (getLastX - event.x) * -1
                         upLastY += (getLastY - event.y) * -1
-                        if (upLastX > 300f * zumm || upLastX < -300f * zumm) {
+                        if (upLastX > 300f * zoom || upLastX < -300f * zoom) {
                             if (upLastX > 0)
-                                upLastX = 299f * zumm
+                                upLastX = 299f * zoom
                             if (upLastX < 0)
-                                upLastX = -299f * zumm
+                                upLastX = -299f * zoom
                         }
-                        if (upLastY > 200f * zumm || upLastY < -200f * zumm) {
+                        if (upLastY > 200f * zoom || upLastY < -200f * zoom) {
                             if (upLastY > 0)
-                                upLastY = 199f * zumm
+                                upLastY = 199f * zoom
                             if (upLastY < 0)
-                                upLastY = -199f * zumm
+                                upLastY = -199f * zoom
                         }
 //                        runOnUiThread {
 //                            Toast.makeText(this@MainActivity, "Up $upLastX", Toast.LENGTH_SHORT).show()
